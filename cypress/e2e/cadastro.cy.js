f481ff0219188189af643298d1726ad3b603ca92
Renata <renatinhasenna@yahoo.com.br>
2024-07-08 17:48:27 -0300

    Refatoração

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,17 +1,48 @@
 /// <reference types="cypress"/>
 
+const { should } = require("chai");
+
+
 describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  beforeEach (() => {
+    cy.visit('/')
+  });
+
   it('Deve fazer o cadastro de campos obrigatórios', () => {
-    cy.visit('http://192.168.0.8:8080/')
-    cy.get('#signup-firstname').type('Renata')
-    cy.get('#signup-lastname').type('Senna')
-    cy.get('#signup-email').type('renata10@test.com')
-    cy.get('#signup-phone').type('1195369635212')
-    cy.get('#signup-password').type('Teste&2020')
-    cy.get('#signup-button').click()
+    var email = `renata${Date.now()}@teste.com`
+    cy.preencherCadastro('Renata', 'Senna', email, '112536987452', 'Teste@12345')
     cy.get('#signup-response').should('contain' , 'Cadastro realizado com sucesso!')
   })
 
+  it('Deve validar mensagem de erro com o campo nome inválido', () => {
+    cy.preencherCadastro('Renata20', 'Senna', 'renata@teste.com', '112536987452', 'Teste@12345')
+    cy.get('#signup-response').should('contain' , 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+  })
+
+  it('Deve validar mensagem de erro com o campo sobrenome inválido', () => {
+    cy.preencherCadastro('Renata', 'Senna20', 'renata@teste.com', '112536987452', 'Teste@12345')
+    cy.get('#signup-response').should('contain' , 'Sobrenome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+  })
+
+  it('Deve validar mensagem de erro com o campo email', () => {
+    cy.preencherCadastro('Renata', 'Senna', 'renatateste.com', '112536987452', 'Teste@12345')
+    cy.get('#signup-response').should('contain' , 'E-mail deve ser um email válido')
+  })  
+
+  it('Deve validar mensagem de erro de senha fraca', () => {
+    cy.preencherCadastro('Renata', 'Senna', 'renata@teste.com', '112536987452', '12345')
+    cy.get('#signup-response').should('contain' , 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial (!@#$&*)' )
+    
+  }) 
+
+  it('Deve validar mensagem de erro de senha vazia', () => {
+    cy.preencherCadastro('Renata', 'Senna', 'renata@teste.com', '112536987452', '')
+    cy.get('#signup-response').should('contain' , 'Senha não pode estar vazia')
+  })
+  
+
+
+/*
 it('Deve validar mensagem de erro com o campo inválido' , () => {
   cy.visit('http://192.168.0.8:8080/')
     cy.get('#signup-firstname').type('123563')
@@ -71,7 +102,7 @@ it('Deve validar mensagem de erro de senha vazia' , () => {
     cy.get('#signup-response').should('contain' , 'O campo de senha não pode estar vazio')
   
 })
-
+*/
 })
 
 
